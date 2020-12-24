a18bebee2bd6d2a8db68de2f571180d2794c0cfb
Vanshika-14 <vanshikakonanki14@gmail.com>
2020-12-24 15:30:38 +0530

    New Code In This File!

--- slingShot.js
+++ slingShot.js
@@ -1,18 +1,28 @@
 class SlingShot {
-   constructor(bodyA, bodyB){
+   constructor(bodyA, pointB){
       var options ={
          bodyA:bodyA,
-         bodyB:bodyB,
+         pointB:pointB,
          stiffness: 0.04,
          length: 10
       }
      this.Sling = Constraint.create (options)
+
+     this.pointB = pointB
+
      World.add(world, this.Sling);
+
    }
+
+   fly(){
+      this.Sling.bodyA = null;
+   }
+
   display() {
-     var pointA = this.Sling.bodyA.position;
-     var pointB = this.Sling.bodyB.position;
-     strokeWeight(4);
-     line(pointA.x, pointA.y, pointB.x, pointB.y);
-  }
-}
+     if(this.Sling.bodyA){
+   var pointA = this.Sling.bodyA.position;
+   var pointB = this.pointB;
+   strokeWeight(4);
+      line(pointA.x, pointA.y, pointB.x, pointB.y);
+     }
+  }}

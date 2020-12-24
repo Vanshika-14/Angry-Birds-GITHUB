3b2cf4e1935b4563b60fd402a6a1dc917fce87ec
Vanshika-14 <vanshikakonanki14@gmail.com>
2020-12-24 15:30:13 +0530

    Removed Log6!

--- sketch.js
+++ sketch.js
@@ -34,11 +34,9 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    log6 = new Log(230,180,80, PI/2);
-
     bird = new Bird(100,100);
 
-    slingshot = new SlingShot(bird.body, log6.body);
+    slingshot = new SlingShot(bird.body, {x: 200, y: 100});
 
 }
 
@@ -63,10 +61,16 @@ function draw(){
     log4.display();
     log5.display();
 
-    log6.display();
-
     bird.display();
     platform.display();
 
     slingshot.display();
+}
+
+function mouseDragged() {
+    Matter.Body.setPosition (bird.body, {x: mouseX, y: mouseY});
+}
+
+function mouseReleased() {
+    slingshot.fly();
 }

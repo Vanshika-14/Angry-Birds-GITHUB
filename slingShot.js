c2644774c3d5dc802d9036e8b4ddcd9183ead096
Vanshika-14 <vanshikakonanki14@gmail.com>
2021-01-09 16:56:58 +0530

    slingShot

--- slingShot.js
+++ slingShot.js
@@ -7,6 +7,9 @@ class SlingShot {
          length: 10
       }
      this.Sling = Constraint.create (options)
+     this.sling1 = loadImage("sprites/sling1.png");
+     this.sling2 = loadImage("sprites/sling2.png");
+     this.sling3 = loadImage("sprites/sling3.png");
 
      this.pointB = pointB
 
@@ -18,11 +21,35 @@ class SlingShot {
       this.Sling.bodyA = null;
    }
 
+   attach(body){
+      this.Sling.bodyA = body;
+   }
+
   display() {
+
+   image(this.sling1, 200, 20);
+   image(this.sling2, 170, 20);
+
      if(this.Sling.bodyA){
    var pointA = this.Sling.bodyA.position;
    var pointB = this.pointB;
+   push();
+   
+   stroke(48, 22, 8);
+
+   if(pointA.x<220){
+   strokeWeight(7);
+      line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+      line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+   image(this.sling3, pointA.x-30, pointA.y-10, 15, 30);
+   }
+   else{
    strokeWeight(4);
-      line(pointA.x, pointA.y, pointB.x, pointB.y);
+      line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+      line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+   image(this.sling3, pointA.x+25, pointA.y-10, 15, 30);
+   }
+
+   pop();
      }
   }}

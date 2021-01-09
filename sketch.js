05b4156288f0e38eaa3c2c211906f1175822b42a
Vanshika-14 <vanshikakonanki14@gmail.com>
2021-01-09 16:52:13 +0530

    Sketch.js Changed

--- sketch.js
+++ sketch.js
@@ -34,18 +34,15 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200, 50);
 
-    slingshot = new SlingShot(bird.body, {x: 200, y: 100});
+    slingshot = new SlingShot(bird.body, {x: 200, y: 50});
 
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -73,4 +70,10 @@ function mouseDragged() {
 
 function mouseReleased() {
     slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
